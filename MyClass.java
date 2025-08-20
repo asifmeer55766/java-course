df5d3f51c392c7bd49681319c772a6299a83de11
Asif Hussain <158553805+asifmeer55766@users.noreply.github.com>
2025-08-20 20:58:49 +0500

    first file

---
+++ MyClass.java
@@ -0,0 +1,14 @@
+class MyClass{
+    public static void main(String args[]){
+        System.out.println("Hello World! ");
+        System.out.println("Welcom to a brand new java course ! ");
+
+
+        for(int i = 1;i<=5;i++){
+            for(int j  = 1; j<=i;j++){
+                 System.out.print('*');
+            }
+                 System.out.println();
+        } 
+    }
+}
